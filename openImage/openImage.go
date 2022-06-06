cf14de221e643b445b68b5df222c4820a7334c2a
Niklas Ohlrogge <niklas.ohlrogge@axzoom.de>
2022-06-06 10:44:37 +0200

    added openImage functionality

---
+++ openImage/openImage.go
@@ -0,0 +1,39 @@
+package openImage
+
+import (
+	"errors"
+	"fmt"
+	"image"
+	_ "image/png"
+	"os"
+)
+
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return (err != nil)
+}
+
+func OpenJpeg(path string) (image.Image, error) {
+
+	var file, err = os.Open(path)
+
+	if isError(err) {
+		return nil, err
+	}
+
+	fmt.Println(file.Name() + " opened")
+	img, format, err := image.Decode(file)
+
+	if isError(err) {
+		return nil, err
+	}
+
+	if format != "jpeg" {
+		fmt.Println("Image format is not jpeg.")
+		return nil, errors.New("image format is not jpeg")
+	}
+
+	return img, nil
+}
