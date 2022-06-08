e6296ddc4b40cd06f7d11e1b29682f88b1a5e3a7
Niklas Ohlrogge <niklas.ohlrogge@axzoom.de>
2022-06-08 12:01:58 +0200

    Refactored main functionality into separate function

--- instasquare.go
+++ instasquare.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
@@ -13,40 +12,40 @@ import (
 	"github.com/ohlrogge/instasquare/openImage"
 )
 
-var path = "samples/sample.jpg"
-
 func main() {
+	var path = "samples/sample.jpg"
+
+	generateSquareImage(path)
+}
+
+func generateSquareImage(path string) {
 
 	img, _ := openImage.OpenJpeg(path)
 
-	fmt.Println(getSquareSideLength(img.Bounds()))
-
 	squareSideLength := getSquareSideLength(img.Bounds())
 
-	canvas := image.NewRGBA(image.Rect(0, 0, squareSideLength, squareSideLength))
+	// Generate
+	squaredCanvas := image.NewRGBA(image.Rect(0, 0, squareSideLength, squareSideLength))
 
-	draw.Draw(canvas, canvas.Bounds(), &image.Uniform{color.RGBA{0, 0, 0, 255}}, image.Pt(0, 0), draw.Src)
+	// Colour canvas
+	draw.Draw(squaredCanvas, squaredCanvas.Bounds(), &image.Uniform{color.RGBA{0, 0, 0, 255}}, image.Pt(0, 0), draw.Src)
 
-	fmt.Println(getStartingPoint(img))
-
-	draw.Draw(canvas, canvas.Bounds(), img, getStartingPoint(img), draw.Src)
+	// Overlay image on the canvas
+	draw.Draw(squaredCanvas, squaredCanvas.Bounds(), img, getStartingPoint(img), draw.Src)
 
 	squareJpeg, _ := os.Create("samples/output.jpg")
-	jpeg.Encode(squareJpeg, canvas, &jpeg.Options{Quality: 90})
+	jpeg.Encode(squareJpeg, squaredCanvas, &jpeg.Options{Quality: 90})
 }
 
 func getSquareSideLength(rectangle image.Rectangle) (squareSideLength int) {
-	if rectangle.Dx() > rectangle.Dy() {
-		return rectangle.Dx()
-	} else {
-		return rectangle.Dy()
-	}
+	return int(math.Max(float64(rectangle.Dx()), float64(rectangle.Dy())))
 }
 
 func getStartingPoint(img image.Image) (startingPoint image.Point) {
 
 	rectangle := img.Bounds()
 
+	// Calculate translation
 	translateBy := -int(math.Abs(math.RoundToEven(float64((rectangle.Dx() - rectangle.Dy()) / 2))))
 
 	if rectangle.Dx() > rectangle.Dy() {
